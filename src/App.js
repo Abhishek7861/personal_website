b83588c465c4190bf43205547c4a339a0586e70e
Abhishek Singh <abhisheksingh1@fynd-external.com>
2023-12-07 17:14:37 +0530

    Boiler Plate code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './pages/home/home';
+import Project from './pages/project/project';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <Project></Project>
+    </div>
+  );
+}
+
+export default App;
