e27dccfad597d0a04cdba8251079a933b9159093
Abhishek7861 <erabhisheksingh@outlook.com>
2023-12-12 23:22:58 +0530

    Added all the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Home from './pages/home/home';
-import Project from './pages/project/project';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Project from './pages/project/project';
+import Experience from './pages/experience/experience';
+import Hobbies from './pages/hobbies/hobbies';
+import Skills from './pages/skills/skills';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route index element={<Home/>} />
           <Route path="/project" element={<Project/>} />
+          <Route path="/experience" element={<Experience/>} />
+          <Route path="/hobbies" element={<Hobbies/>} />
+          <Route path="/skills" element={<Skills/>} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/hobbies/hobbies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+
+export default function hobbies() {
+  return (
+    <div>
+        <NavBar></NavBar>
+      hobbies
+    </div>
+  )
+}
